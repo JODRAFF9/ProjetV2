90c1987ab4aff97e5df72eebbc618c5a99e989fc
JODRAFF <traoresierachid@gmail.com>
2025-06-07 23:37:22 +0000

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ from pages.accueil import accueil
 from pages.analyse import analyse
 from pages.prediction import prediction
 from pages.apropos import apropos
-from funcs import load_data,map,style
+from funcs import load_data, map, style
 
 # Configuration de la page Streamlit
 st.set_page_config(page_title="Prédiction du BANK CHURN ",page_icon="docs/icon.png" , layout="wide")
